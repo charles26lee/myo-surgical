0b1286e287003ddbfe5b7d5b6d3784d1a6797a8e
charles <charles26lee@gmail.com>
2016-04-13 09:39:05 +0900

    omitted video recording to stop lag

--- public/javascripts/webcamManager.js
+++ public/javascripts/webcamManager.js
@@ -1,3 +1,7 @@
+/* window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame;
+window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame || window.msCancelAnimationFrame || window.oCancelAnimationFrame;
+
+var rafId = null;
 var frames = new Array();
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
@@ -5,11 +9,15 @@ var CANVAS_HEIGHT = canvas.height;
 var CANVAS_WIDTH = canvas.width;
 
 var recordVideo = function() {
+	rafId = requestAnimationFrame(drawVideoFrame);
+}
+
+var drawVideoFrame = function(time) {
 	ctx.drawImage(document.getElementById("videoElement"), 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 	
 	var url = canvas.toDataURL("image/webp", 1);
 	frames.push(url);
-}
+} */
 
 $(document).ready(function() {
 	var video = document.querySelector("#videoElement");
